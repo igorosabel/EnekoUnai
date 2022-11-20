578eb1c368cbe8c8b2847fa19c6f16c625dfca56
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-20 15:06:55 +0100

    Actualizo Angular CLI (14.2.9 -> 15.0.0), Angular (14.2.10 -> 15.0.0), Angular Material (14.2.7 -> 15.0.0) y  Angular CDK (14.2.7 -> 15.0.0). Correcciones por cambios de diseño.

--- src/app/app.common.ts
+++ src/app/app.common.ts
@@ -1,22 +1,22 @@
 /**
  * Páginas
  */
-import { InicioComponent }         from './pages/inicio/inicio.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 import { ListadoCuentosComponent } from './pages/cuentos/listado-cuentos/listado-cuentos.component';
-import { ZenaComponent }           from './pages/cuentos/zena/zena.component';
-import { TomasaComponent }         from './pages/cuentos/tomasa/tomasa.component';
-import { ListadoJuegosComponent }  from './pages/juegos/listado-juegos/listado-juegos.component';
-import { MemoryComponent }         from './pages/juegos/memory/memory.component';
-import { SimonComponent }          from './pages/juegos/simon/simon.component';
+import { ZenaComponent } from './pages/cuentos/zena/zena.component';
+import { TomasaComponent } from './pages/cuentos/tomasa/tomasa.component';
+import { ListadoJuegosComponent } from './pages/juegos/listado-juegos/listado-juegos.component';
+import { MemoryComponent } from './pages/juegos/memory/memory.component';
+import { SimonComponent } from './pages/juegos/simon/simon.component';
 
 export const PAGES: any[] = [
-	InicioComponent,
-	ListadoCuentosComponent,
-	ZenaComponent,
-	TomasaComponent,
-	ListadoJuegosComponent,
-	MemoryComponent,
-	SimonComponent
+  InicioComponent,
+  ListadoCuentosComponent,
+  ZenaComponent,
+  TomasaComponent,
+  ListadoJuegosComponent,
+  MemoryComponent,
+  SimonComponent,
 ];
 
 /**
@@ -24,9 +24,7 @@ export const PAGES: any[] = [
  */
 import { HeaderComponent } from './components/header/header.component';
 
-export const COMPONENTS: any[] = [
-	HeaderComponent
-];
+export const COMPONENTS: any[] = [HeaderComponent];
 
 /**
  * Pipes
@@ -44,13 +42,13 @@ export const SERVICES: any[] = [];
  * Componentes Angular Material
  */
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule }    from '@angular/material/card';
-import { MatButtonModule }  from '@angular/material/button';
-import { MatIconModule }    from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 export const MATERIAL: any[] = [
-	MatToolbarModule,
-	MatCardModule,
-	MatButtonModule,
-	MatIconModule
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule,
 ];
